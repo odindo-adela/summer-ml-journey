1307aecec7d5d03378ed08585f14b8c22d09261d
Adela <odindo.adela@gmail.com>
2026-05-12 20:59:47 +0300

    Day 3: Added menu system and calculation history.

--- calculator2.0.py
+++ calculator2.0.py
@@ -22,12 +22,38 @@ def calculator(num1,operator,num2):
     return result
 
 while True:
-    num1 = float(input("Enter first number: "))
-    operator = input("Choose your operation(+,-,/,*): ")
-    num2 = float(input("Enter second number: "))
-    result = calculator(num1, operator,num2)
-    print(result)
+    print("\nCalculation Menu:")
+    print("Option 1. Calculate")
+    print("Option 2. View History")
+    print("Option 3. Exit")
+    choice= input("Choose an option(1, 2 or 3):")
 
-    again=input("Do you want to continue?(yes/no): ")
-    if again.lower() == "no":
+    if choice=="1":
+        num1 = float(input("Enter first number: "))
+        operator = input("Choose your operation(+,-,/,*): ")
+        num2 = float(input("Enter second number: "))
+        result = calculator(num1, operator,num2)
+        
+        if isinstance(result, (int,float)):
+            with open("history.txt", "a") as file:
+                file.write(f"{num1} {operator} {num2} = {result}\n")
+
+            
+        print("Result:", result)
+
+    elif choice=="2":
+        with open("history.txt", "r") as file:
+            history = file.read()
+            if history.strip():
+                print("Calculation History:")
+                print(history)
+            else:
+                print("History is empty")
+                
+
+    elif choice=="3":
+        print("Exiting Calculator, Thank you!")
         break
+
+    else:
+        print("Invalid choice! Kindly select 1, 2 or 3")
