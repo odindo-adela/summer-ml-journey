13c38bfe0954d4d8a55653e84a01c5d13ff98f7a
Adela <odindo.adela@gmail.com>
2026-05-22 12:06:39 +0300

    Day 13: Built first classification model.

---
+++ classification_intro.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_absolute_error
+
+df = pd.read_csv("soccer_player.csv")
+
+X = df[["Goals", "Assists", "Matches"]]
+y = df[["Elite"]]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LogisticRegression()
+
+model.fit(X_train, y_train)
+
+prediction = model.predict([[15, 2, 20]])
+print(prediction)
+
+accuracy = model.score(X_test, y_test)
+print("Accuracy: ", accuracy)
+
+
+
