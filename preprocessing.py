239a2a29e0a121b3800554ad22822bb11abf51eb
Adela <odindo.adela@gmail.com>
2026-05-25 15:44:50 +0300

    Day 15: Practiced Feature engineering and model comparison.

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("messy_soccer_data.csv")
 
@@ -23,7 +25,10 @@ df["Position"] = df["Position"].map({
 })
 print(df)
 
-X = df[["Assists", "Matches", "Position"]]
+df["Goals_Per_Match"] = df["Goals"] / df["Matches"]
+print(df)
+
+X = df[["Assists", "Matches", "Position", "Goals_Per_Match"]]
 y = df["Goals"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
@@ -34,3 +39,9 @@ model.fit(X_train, y_train)
 prediction = model.predict(X_test)
 print("Predictions: ", prediction )
 print("Actual Values:", y_test.values)
+
+error = mean_absolute_error(y_test, prediction)
+print("Mean Absolute Error: ", error)
+
+plt.scatter(df["Goals_Per_Match"], df["Goals"])
+plt.show()
