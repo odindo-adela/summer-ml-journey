4562a7cc1f17b8a563d2fbb1de1b9adea17e66dc
Adela <odindo.adela@gmail.com>
2026-06-02 12:43:40 +0300

    Day 20: Evaluated classification model with confusion matrix and classification report.

--- classification_model.py
+++ classification_model.py
@@ -2,10 +2,12 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score 
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 
 df = pd.read_csv("soccer_player.csv")
 
-df["Elite Player"] = df["Goals"] > 25
+df["Elite Player"] = df["Goals"] > 20
 
 X = df[["Matches", "Assists"]]
 y = df["Elite Player"]
@@ -26,3 +28,8 @@ print("Accuracy: ", accuracy)
 
 probability = model.predict_proba(X_test)
 print("Probability: ", probability)
+
+cm = confusion_matrix(y_test, predictions)
+print("Confusion Matrix: \n", cm)
+
+print(classification_report(y_test, predictions))
